2c7bd534da9b3285d45b469596edb11fb040d649
orange_hehe <1076017537@qq.com>
2017-07-03 23:10:58 +0800

    modify tables

--- server/application/Common/Conf/config.php
+++ server/application/Common/Conf/config.php
@@ -20,7 +20,7 @@ $configs= array(
         "LOAD_EXT_FILE"=>"extend",
         'UPLOADPATH' => 'data/upload/',
         //'SHOW_ERROR_MSG'        =>  true,    // 显示错误信息
-        'SHOW_PAGE_TRACE'		=> false,
+        'SHOW_PAGE_TRACE'		=> true,
         'TMPL_STRIP_SPACE'		=> true,// 是否去除模板文件里面的html空格与换行
         'THIRD_UDER_ACCESS'		=> false, //第三方用户是否有全部权限，没有则需绑定本地账号
         /* 标签库 */
@@ -28,7 +28,7 @@ $configs= array(
         'MODULE_ALLOW_LIST'  => array('Admin','Portal','Asset','Api','User','Wx','Comment','Qiushi','Tpl','Topic','Install','Bug','Better','Pay','Cas'),
         'TMPL_DETECT_THEME'     => false,       // 自动侦测模板主题
         'TMPL_TEMPLATE_SUFFIX'  => '.html',     // 默认模板文件后缀
-        'DEFAULT_MODULE'        =>  'Forum',  // 默认模块
+        'DEFAULT_MODULE'        =>  'Portal',  // 默认模块
         'DEFAULT_CONTROLLER'    =>  'Index', // 默认控制器名称
         'DEFAULT_ACTION'        =>  'index', // 默认操作名称
         'DEFAULT_M_LAYER'       =>  'Model', // 默认的模型层名称
@@ -50,7 +50,7 @@ $configs= array(
         'APP_AUTOLOAD_LAYER'    =>  'Controller,Model', // 模块自动加载的类库后缀
         
         'SP_TMPL_PATH'     		=> 'themes/',       // 前台模板文件根目录
-        'SP_DEFAULT_THEME'		=> 'forum',       // 前台模板文件
+        'SP_DEFAULT_THEME'		=> 'simplebootx',       // 前台模板文件
         'SP_TMPL_ACTION_ERROR' 	=> 'error', // 默认错误跳转对应的模板文件,注：相对于前台模板路径
         'SP_TMPL_ACTION_SUCCESS' 	=> 'success', // 默认成功跳转对应的模板文件,注：相对于前台模板路径
         'SP_ADMIN_STYLE'		=> 'flat',
@@ -72,7 +72,7 @@ $configs= array(
         
         /* URL设置 */
         'URL_CASE_INSENSITIVE'  => true,   // 默认false 表示URL区分大小写 true则表示不区分大小写
-        'URL_MODEL'             => 0,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+        'URL_MODEL'             => 2,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
         // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式，提供最好的用户体验和SEO支持
         'URL_PATHINFO_DEPR'     => '/',	// PATHINFO模式下，各参数之间的分割符号
         'URL_HTML_SUFFIX'       => '',  // URL伪静态后缀设置
