6ef919721172dfcd7198d288f415e15c25ac5aa0
orange_hehe <1076017537@qq.com>
2017-08-23 22:37:26 +0800

    add: news component

---
+++ client/ps4forum/src/components/common/ps-posts-list.vue
@@ -0,0 +1,171 @@
+<template>
+	<div>
+		<div class="list-group-item">
+      <div class="row" style="padding: 10px 15px;">
+          <div class="col-md-9" style="padding: 0;margin: 0;">
+              <h4 class="list-group-item-heading">
+                  <a href="newsDetail.html">PS4《英雄传说 闪之轨迹3》公布在帝国阴影中活动的神秘人物 — 「苍」...</a>
+              </h4>
+              <p class="list-group-item-text">
+                  萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带
+              </p>
+              <div class="clearfix img-gather" id="thumbs">
+                  <a href="../../assets/images/b1.jpg" :style="{'background-image': 'url(../../assets/images/b1.jpg)'}" title="血源诅咒"></a>
+                  <a href="../../assets/images/b2.jpg" :style="{'background-image': 'url(../../assets/images/b2.jpg)'}" title="血源诅咒"></a>
+                  <a href="../../assets/images/b3.jpg" :style="{'background-image': 'url(../../assets/images/b3.jpg)'}" title="血源诅咒"></a>
+                  <a href="../../assets/images/b6.jpg" :style="{'background-image': 'url(../../assets/images/b6.jpg)'}" title="血源诅咒"></a>
+              </div>
+          </div>
+          <div class="col-md-3">
+              <p class="posts-lists-right">
+                  <a href="#">
+                      <img width="34px" height="34px" class="ava" src="../../assets/images/avator.png" alt="">
+                  </a>
+                  <a href="#">卑鄙异乡人</a>
+              </p>
+              <p class="posts-lists-right">
+                  <a href="#"><span>回复（13）</span></a>
+              </p>
+              <p class="posts-lists-right">
+                  <span>33分钟前</span>
+              </p>
+          </div>
+      </div>
+  	</div>
+	  <div class="list-group-item">
+	      <div class="row" style="padding: 10px 15px;">
+	          <div class="col-md-9" style="padding: 0;margin: 0;">
+	              <h4 class="list-group-item-heading">
+	                  <a href="newsDetail.html">《真三国无双8》官网今日（8.10）公布大量新图，包括桃园三结义...</a>
+	              </h4>
+	              <p class="list-group-item-text">
+	                  《真三国无双8》官网今日（8.10）公布大量新图，包括桃园三结义、董卓与貂蝉、吕布大战张飞、夏侯惇单挑关羽、蜀吴同盟等。
+	              </p>
+	          </div>
+	          <div class="col-md-3">
+	              <p class="posts-lists-right">
+	                  <a href="#">
+	                      <img width="34px" height="34px" class="ava" src="../../assets/images/avator.png" alt="">
+	                  </a>
+	                  <a href="#">卑鄙异乡人</a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <a href="#"><span>回复（13）</span></a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <span>35分钟前</span>
+	              </p>
+	          </div>
+	      </div>
+	  </div>
+	  <div class="list-group-item">
+	      <div class="row" style="padding: 10px 15px;">
+	          <div class="col-md-9" style="padding: 0;margin: 0;">
+	              <h4 class="list-group-item-heading">
+	                  <a href="newsDetail.html">《真三国无双8》官网今日（8.10）公布大量新图，包括桃园三结义...</a>
+	              </h4>
+	              <p class="list-group-item-text">
+	                  萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带 萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带
+	              </p>
+	          </div>
+	          <div class="col-md-3">
+	              <p class="posts-lists-right">
+	                  <a href="#">
+	                      <img width="34px" height="34px" class="ava" src="../../assets/images/avator.png" alt="">
+	                  </a>
+	                  <a href="#">卑鄙异乡人</a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <a href="#"><span>回复（13）</span></a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <span>2017-06-30</span>
+	              </p>
+	          </div>
+	      </div>
+	  </div>
+	  <div class="list-group-item">
+	      <div class="row" style="padding: 10px 15px;">
+	          <div class="col-md-9" style="padding: 0;margin: 0;">
+	              <h4 class="list-group-item-heading">
+	                  <a href="newsDetail.html">《真三国无双8》官网今日（8.10）公布大量新图，包括桃园三结义...</a>
+	              </h4>
+	              <p class="list-group-item-text">
+	                  萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带 萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带
+	              </p>
+	          </div>
+	          <div class="col-md-3">
+	              <p class="posts-lists-right">
+	                  <a href="#">
+	                      <img width="34px" height="34px" class="ava" src="../../assets/images/avator.png" alt="">
+	                  </a>
+	                  <a href="#">卑鄙异乡人</a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <a href="#"><span>回复（13）</span></a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <span>2017-06-30</span>
+	              </p>
+	          </div>
+	      </div>
+	  </div>
+	  <div class="list-group-item">
+	      <div class="row" style="padding: 10px 15px;">
+	          <div class="col-md-9" style="padding: 0;margin: 0;">
+	              <h4 class="list-group-item-heading">
+	                  <a href="newsDetail.html">《真三国无双8》官网今日（8.10）公布大量新图，包括桃园三结义...</a>
+	              </h4>
+	              <p class="list-group-item-text">
+	                  萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带 萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带
+	              </p>
+	          </div>
+	          <div class="col-md-3">
+	              <p class="posts-lists-right">
+	                  <a href="#">
+	                      <img width="34px" height="34px" class="ava" src="../../assets/images/avator.png" alt="">
+	                  </a>
+	                  <a href="#">卑鄙异乡人</a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <a href="#"><span>回复（13）</span></a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <span>2017-06-30</span>
+	              </p>
+	          </div>
+	      </div>
+	  </div>
+	  <div class="list-group-item">
+	      <div class="row" style="padding: 10px 15px;">
+	          <div class="col-md-9" style="padding: 0;margin: 0;">
+	              <h4 class="list-group-item-heading">
+	                  <a href="newsDetail.html">《真三国无双8》官网今日（8.10）公布大量新图，包括桃园三结义...</a>
+	              </h4>
+	              <p class="list-group-item-text">
+	                  萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带 萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带
+	              </p>
+	          </div>
+	          <div class="col-md-3">
+	              <p class="posts-lists-right">
+	                  <a href="#">
+	                      <img width="34px" height="34px" class="ava" src="../../assets/images/avator.png" alt="">
+	                  </a>
+	                  <a href="#">卑鄙异乡人</a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <a href="#"><span>回复（13）</span></a>
+	              </p>
+	              <p class="posts-lists-right">
+	                  <span>2017-06-30</span>
+	              </p>
+	          </div>
+	      </div>
+	  </div>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'ps-posts-list'
+	}
+</script>
--- client/ps4forum/src/views/ps-newsList.vue
+++ client/ps4forum/src/views/ps-newsList.vue
@@ -1,8 +1,51 @@
 <template>
-	<h3>news list</h3>
+  <div class="container" id="main-body" style="background-color: #fff;max-width: 970px;">
+      <div class="row">
+          <form class="bs-example bs-example-form" role="form" style="margin-top: 35px;">
+              <div class="col-md-6 col-md-push-3">
+                  <div class="input-group">
+                      <input type="text" class="form-control" placeholder="搜新闻...">
+                      <span class="input-group-btn">
+                          <button class="btn btn-primary" type="button">搜索</button>
+                      </span>
+                  </div>
+              </div>
+          </form>
+          <div class="col-md-12 posts-lists" style="margin-top: 30px;">
+							<ps-posts-list></ps-posts-list>
+              <ul class="pagination">
+                  <li><a href="#">&laquo;</a></li>
+                  <li class="active"><a href="#">1</a></li>
+                  <li><a href="#">2</a></li>
+                  <li><a href="#">3</a></li>
+                  <li><a href="#">4</a></li>
+                  <li><a href="#">5</a></li>
+                  <li><a href="#">&raquo;</a></li>
+              </ul>
+          </div>
+      </div>
+  </div>
 </template>
 <script>
+	import '@/assets/js/touchTouch.jquery.js'
+	import psPostsList from '@/components/common/ps-posts-list'
+	
 	export default {
-		name: 'ps-newsList'
+		name: 'ps-newsList',
+		components: {
+			psPostsList
+		}
 	}
+
+  $('#thumbs a').touchTouch()
+  $('.posts-lists .list-group-item').hover(function() {
+      $(this).addClass('posts-list-hover')
+  }, function() {
+      $(this).removeClass('posts-list-hover')
+  });
 </script>
+<style>
+	@import '../assets/css/touchTouch.css';
+	@import '../assets/css/game-thumb-img.css';
+	@import '../assets/css/newsList.css';
+</style>
