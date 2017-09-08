704554ffc35f8faad65523caf16f72f4397687e7
bohehe <hehebu107601@foxmail.com>
2017-09-08 17:37:14 +0800

    fix: u-game

--- client/ps4forum/src/views/ucenter/ps-user-center-game.vue
+++ client/ps4forum/src/views/ucenter/ps-user-center-game.vue
@@ -1,6 +1,26 @@
 <template>
-	<div>
-		<h3>his game</h3>
+	<div class="col-md-10 col-md-push-1 user-posts">
+    <div class="list-group-item">
+        <div class="row" style="padding: 10px 15px;">
+            <div class="col-md-9" style="padding: 0;margin: 0;">
+                <h4 class="list-group-item-heading">
+                    <a href="#">一周目被宇宙之女打成象拔蚌，求大佬带带~~</a>
+                </h4>
+                <p class="list-group-item-text">
+                    萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带
+                </p>
+            </div>
+            <div class="col-md-3">
+                <p class="posts-lists-right">
+                    <a href="#"><span>回复（13）</span></a>
+                    <button class="btn btn-danger">删除</button>
+                </p>
+                <p class="posts-lists-right">
+                    <span>33分钟前</span>
+                </p>
+            </div>
+        </div>
+    </div>
 	</div>
 </template>
 <script>
