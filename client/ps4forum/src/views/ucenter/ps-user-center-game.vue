a8433d7b34e0867f7fcaa3f5e036e2e5e7d1d0d2
bohehe <hehebu107601@foxmail.com>
2017-09-09 19:02:00 +0800

    fix: u-game

--- client/ps4forum/src/views/ucenter/ps-user-center-game.vue
+++ client/ps4forum/src/views/ucenter/ps-user-center-game.vue
@@ -1,25 +1,68 @@
 <template>
 	<div class="col-md-10 col-md-push-1 user-posts">
     <div class="list-group-item">
-        <div class="row" style="padding: 10px 15px;">
-            <div class="col-md-9" style="padding: 0;margin: 0;">
-                <h4 class="list-group-item-heading">
-                    <a href="#">一周目被宇宙之女打成象拔蚌，求大佬带带~~</a>
-                </h4>
-                <p class="list-group-item-text">
-                    萌新第一次玩血源，跟着黑桐谷歌的教程一直打到宇宙之女，实在太难了打不过，求来个大佬带带
-                </p>
-            </div>
-            <div class="col-md-3">
-                <p class="posts-lists-right">
-                    <a href="#"><span>回复（13）</span></a>
-                    <button class="btn btn-danger">删除</button>
-                </p>
-                <p class="posts-lists-right">
-                    <span>33分钟前</span>
-                </p>
-            </div>
+      <div class="row" style="padding: 10px 15px;">
+        <div class="pull-left col-md-2">
+          <router-link to="#">
+            <img width="90" height="50" src="../../assets/images/b1.jpg">
+          </router-link>
         </div>
+        <div class="pull-left col-md-3 u-game-title">
+          <router-link to="#">血源诅咒</router-link><br>
+          <span>2周前</span>
+        </div>
+        <div class="col-md-2 pull-right u-game-info">
+          <p class="posts-lists-right text-center">
+            <a href="#"><span>麻烦</span></a>
+          </p>
+          <p class="posts-lists-right text-center">
+            <span>21.62%完美</span>
+          </p>
+        </div>
+        <div width="16%" class="pd10">
+          <div class="progress">
+            <div class="progress-len" style="width:42%">42%</div>
+          </div>
+          <small class="h-p">
+            <span class="text-platinum">白0</span>
+            <span class="text-gold">金1</span>
+            <span class="text-silver">银6</span>
+            <span class="text-bronze">铜17</span>
+          </small>
+        </div>
+      </div>
+    </div>
+    <div class="list-group-item">
+      <div class="row" style="padding: 10px 15px;">
+        <div class="pull-left col-md-2">
+          <router-link to="#">
+            <img width="90" height="50" src="../../assets/images/b1.jpg">
+          </router-link>
+        </div>
+        <div class="pull-left col-md-3 u-game-title">
+          <router-link to="#">血源诅咒</router-link><br>
+          <span>2周前</span>
+        </div>
+        <div class="col-md-2 pull-right u-game-info">
+          <p class="posts-lists-right text-center">
+            <a href="#"><span>麻烦</span></a>
+          </p>
+          <p class="posts-lists-right text-center">
+            <span>21.62%完美</span>
+          </p>
+        </div>
+        <div width="16%" class="pd10">
+          <div class="progress">
+            <div class="progress-len" style="width:42%">42%</div>
+          </div>
+          <small class="h-p">
+            <span class="text-platinum">白0</span>
+            <span class="text-gold">金1</span>
+            <span class="text-silver">银6</span>
+            <span class="text-bronze">铜17</span>
+          </small>
+        </div>
+      </div>
     </div>
 	</div>
 </template>
@@ -28,3 +71,15 @@
 		name: 'ps-user-center-game'
 	}
 </script>
+<style>
+  .user-posts .list-group-item {
+    border: none;
+  }
+  .u-game-title {
+    padding: 5px 0;
+  }
+  .progress-len {
+    background-color: #faa732;
+    color: #fff;
+  }
+</style>
